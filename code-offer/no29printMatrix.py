ee8c9b4098ac57540f33abb566f7988a20eb3303
Jacyn <liujiaxin@pku.edu.cn>
2020-02-25 22:43:53 +0800

    顺时针打印矩阵v2

--- code-offer/no29printMatrix.py
+++ code-offer/no29printMatrix.py
@@ -11,3 +11,49 @@ def printMatrix(matrix):
 
 # matrix and [*matrix.pop(0)]是为了防止matrix为空时进行pop出错，and短路机制：matrix为[]时不再执行and后的语句。
 # [::-1]每次都要进行旋转
+
+def printMatrixClockwisely(matrix):
+    if not matrix or len(matrix) <= 0 or len(matrix[0]) <= 0:
+        return
+
+    start = 0
+    rows = len(matrix)
+    cols = len(matrix[0])
+    res = []
+
+    while(cols > start *2 and rows > start * 2):
+        printMatrixInCircle(matrix,cols,rows,start,res)
+        start += 1
+
+    return res
+
+def printMatrixInCircle(matrix,columns,rows,start,res):
+    endX = columns - start -1
+    endY = rows - start -1
+
+    # 从左到右打印第一行
+    for i in range(start,endX+1):
+        res.append(matrix[start][i])
+
+    # 从上到下打印一列,，需要这一步的条件是至少有两行
+    if start < endY:
+        # 这一步的第一个也是上一步的最后一个，start+1
+        for i in range(start+1,endY+1):
+            res.append(matrix[i][endX])
+
+    # 从右到左打印，需要这一步的条件是至少有两行两列
+    if start < endX and start < endY:
+        # 因为这一步的第一个是上一步的最后一个，因此这里的第一个是endx-1
+        for i in range(endX-1,start-1,-1):
+            res.append(matrix[endY][i])
+
+    # 从下往上，需要这一步的条件是至少有三行两列
+    if start < endX and start < endY -1:
+        for i in range(endY-1,start,-1):
+            res.append(matrix[i][start])
+
+if __name__ == '__main__':
+    matrix = [[1],[2],[3],[4],[5]]
+    print(printMatrixClockwisely(matrix))
+
+

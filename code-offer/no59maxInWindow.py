37fd24bb10ba6289c455fa3a62e35092da0944d4
Jacyn <liujiaxin@pku.edu.cn>
2020-03-29 00:49:57 +0800

    滑动窗口的最大值

--- code-offer/no59maxInWindow.py
+++ code-offer/no59maxInWindow.py
@@ -1,28 +1,32 @@
 def maxInWindow(num,size):
-    if not num or size<=0 or len(num) <= 0 :
+    if not num or size<=0 or len(num) <= 0 or len(num)<size:
         return []
     res = []
     window = []
-    maxNum = -1
 
-    window.append(num[0])
+
+    window.append(0)
     for i in range(1,size):
 
-        if num[i] > maxNum:
+        if num[i] > num[window[0]]:
             window.pop(0)
+        # 注意保存的是下标
         window.append(i)
 
     for i in range(size,len(num)):
 
         res.append(num[window[0]])
-
+        # 这里是从后边的小的开始比较，一个个把当前窗口内比num[i]小的删掉
         while window and num[i] >= num[window[-1]]:
             window.pop()
 
         if window and window[0]  <= i - size:
             window.pop(0)
         window.append(i)
+    res.append(num[window[0]])
 
     return res
+
+print(maxInWindow([2,3,4,2,6,2,5,1],3))
 
 
